023654ec0a1f2e580841b66afc2a608df26377fb
siekmeierpaul <siekmeierpaul@gmail.com>
2020-07-05 17:45:11 -0500

    csv read success

---
+++ PyBank/main.py
@@ -0,0 +1,13 @@
+import os
+import csv
+
+bank_csv = os.path.join('Resources', 'budget_data.csv')
+a = 0
+with open(bank_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    print(f"CSV Header:  {csv_header}")
+    for row in csvreader:
+        a = a + 1
+        print(row)
+    print(a)
