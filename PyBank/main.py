1617a561cd436136de42f161df42c024af9ddc52
siekmeierpaul <siekmeierpaul@gmail.com>
2020-07-08 21:22:54 -0500

    results writing

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,13 +1,44 @@
 import os
 import csv
 
-bank_csv = os.path.join('Resources', 'budget_data.csv')
+# set file paths
+budget_data = os.path.join('Resources', 'budget_data.csv')
+results_file_path = os.path.join('analysis', 'results.txt')
+
 a = 0
-with open(bank_csv, 'r') as csvfile:
+months = {}
+with open(budget_data, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
     print(f"CSV Header:  {csv_header}")
     for row in csvreader:
+        months[row[0]] = int(row[1])
         a = a + 1
-        print(row)
+        # print(row)
     print(a)
+    print(len(months))
+    print(months.values())
+    print(sum(months.values()))
+
+# write results to terminal
+print('\n')
+print('Financial Analysis')
+print('-----------------------------------')
+print(f'Total Months: ' + '')
+print(f'Total: ' + '')
+print(f'Average Change: ' + '')
+print(f'Greatest Increase in Profits: ' + '')
+print(f'Greatest Decrease in Profits: ' + '')
+print('\n')
+
+# write results to file
+with open(results_file_path, 'w') as results:
+    results.write('Financial Analysis\n')
+    results.write('-----------------------------------\n')
+    results.write('Total Months: ' + '\n')
+    results.write('Total: ' + '\n')
+    results.write('Average Change: ' + '\n')
+    results.write('Greatest Increase in Profits: ' + '\n')
+    results.write('Greatest Decrease in Profits: ' + '\n')
+    results.write('\n')
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,13 +1,50 @@
 import os
 import csv
 
-bank_csv = os.path.join('Resources', 'election_data.csv')
+# set file paths
+election_data = os.path.join('Resources', 'election_data.csv')
+results_file_path = os.path.join('analysis', 'results.txt')
+
 a = 0
-with open(bank_csv, 'r') as csvfile:
+votes = {}
+with open(election_data, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
     print(f"CSV Header:  {csv_header}")
     for row in csvreader:
+        votes[row[0]] = row[2]
         a = a + 1
         # print(row)
     print(a)
+    print(len(votes))
+    receiving_votes = set(votes.values())
+    candidates = {}
+    for receiver_of_votes in receiving_votes:
+        candidates[receiver_of_votes] = [voter for voter,cand in votes.items() if cand == receiver_of_votes]
+    
+    for candidate, voters in candidates.items():
+        print(f"{candidate} has {len(voters)}")
+    
+# write results to terminal
+print('\n')
+print('Election Results')
+print('-----------------------------------')
+print('Total Votes: ' + '')
+print('-----------------------------------')
+print(':' + '')
+print('-----------------------------------')
+print('Winner: ' + '')
+print('-----------------------------------')
+print('\n')
+
+# write results to file
+with open(results_file_path, 'w') as results:
+    results.write('Election Results\n')
+    results.write('-----------------------------------\n')
+    results.write('Total Votes: ' + '\n')
+    results.write('-----------------------------------\n')
+    results.write(':' + '\n')
+    results.write('-----------------------------------\n')
+    results.write('Winner: ' + '\n')
+    results.write('-----------------------------------\n')
+    results.write('\n')
